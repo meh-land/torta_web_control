cc3b8db2b71b9b4c24c6305032c9c40c74e96f5c
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-05-08 13:13:22 +0300

    ziad's pub

--- src/arm_pub.cpp
+++ src/arm_pub.cpp
@@ -1,45 +1,62 @@
-#include <ros/ros.h>
-#include <std_msgs/Float32.h>
-#include <chrono>
-#include <thread>
+#include "ros/ros.h"
+#include "std_msgs/Int32MultiArray.h"
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "arm_pubcpp");
-    ros::NodeHandle n;
+int main(int argc, char **argv) {
+    // Initialize the ROS node
+    ros::init(argc, argv, "Arm_Positions");
 
-    ros::Publisher arm_pub = n.advertise<std_msgs::Float32>("cmd_arm", 10);
-    ros::Rate loop_rate(10);
+    // Create a ROS NodeHandle
+    ros::NodeHandle nh;
 
-    std_msgs::Float32 arm_msg;
+    // Create a publisher object
+    ros::Publisher pub = nh.advertise<std_msgs::Int32MultiArray>("Arm_Positions_Publisher", 10);
 
-    while (arm_pub.getNumSubscribers() == 0)
-    {
-        ROS_INFO("Waiting for a subscriber to the 'cmd_arm' topic...");
-        ros::Duration(0.2).sleep();
+
+    // Set the loop rate (in Hz)
+    ros::Rate loop_rate(1); // 1 Hz
+
+    // Counter variable
+    int count = 0;
+    int grip=0;
+    while (ros::ok()) {
+        // Create a message object
+        std_msgs::Int32MultiArray msg;
+
+        // Set the array size to 3
+        msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
+        msg.layout.dim[0].size = 3;
+        msg.layout.dim[0].stride = 3;
+        msg.layout.dim[0].label = "Arm_Positions";
+
+        // Add the integer values to the data array
+        msg.data.clear();
+        msg.data.push_back(0);
+        msg.data.push_back(0);
+        msg.data.push_back(grip);
+	
+        // Publish the message
+        pub.publish(msg);
+
+        // Log the published values
+        ROS_INFO("Published: [%d, %d, %d]", msg.data[0], msg.data[1], msg.data[2]);
+
+        // Increment the counter
+        count += 45; // Increment by 3 for the next set of values
+        if (grip == 0)
+        {
+        	grip = 1;
+        }
+        else
+        {
+        	grip = 0;
+        }
+        
+        // Spin once
+        ros::spinOnce();
+
+        // Sleep to maintain the loop rate
+        loop_rate.sleep();
     }
-
-    if (argc > 1)
-    {
-        arm_msg.data = atof(argv[1]);
-    }
-    else
-    {
-        arm_msg.data = 0.0;
-        ROS_INFO("No value provided so we assume value is zero ");
-
-    }
-
-    ROS_INFO_STREAM(arm_msg.data);
-
-    arm_pub.publish(arm_msg);
-
-    // Process pending callbacks (optional)
-    ros::spinOnce();
-
-    // Sleep for the remaining time to maintain the desired loop rate
-    loop_rate.sleep();
-
 
     return 0;
 }
