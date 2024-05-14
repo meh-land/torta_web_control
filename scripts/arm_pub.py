531d4c5751820f80f6b5065350b09485f00907b2
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-05-14 23:28:13 +0300

    arm pub

--- scripts/arm_pub.py
+++ scripts/arm_pub.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 import rospy
 import sys
-from std_msgs.msg import Float32
+from std_msgs.msg import Int16MultiArray
 
 # PID publisher
 def arm_pub():
-    pub = rospy.Publisher("cmd_arm", Float32, queue_size=10)
+    pub = rospy.Publisher("arm_joints", Int16MultiArray, queue_size=10)
     rospy.init_node("arm_pub", anonymous=True)
 
     # Get arm values from arguments
     try:
-        arm_val = float(sys.argv[1])
+        arm_val1 = int(sys.argv[1])
     except:
-        arm_val = 0
-   
-    # Publish recieved values
-    arm_str = str(arm_val)
+        arm_val1 = 0
+    try:
+        arm_val2 = int(sys.argv[2])
+    except:
+        arm_val2 = 0
+    # Create an Int16MultiArray message
+    arm_msg = Int16MultiArray()
 
-    rospy.loginfo(arm_str)
-    pub.publish(arm_val)
+    # Fill the data array with some example values
+    arm_msg.data = [arm_val1, arm_val2]
+
+    rospy.loginfo(arm_msg)
+    pub.publish(arm_msg)
 
 if __name__ == '__main__':
     try:
