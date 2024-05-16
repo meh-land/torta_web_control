fc5cda4a78e8570f9504a5f57180e895ec8982ab
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-05-16 18:05:15 +0300

    robotics arm pub

--- scripts/arm_pub.py
+++ scripts/arm_pub.py
@@ -3,18 +3,39 @@ import rospy
 import sys
 from std_msgs.msg import Int16MultiArray
 
-# PID publisher
+Pmax = 105
+Pmin = 65
+Rmax = 170
+Rmin = 10
+
+def constraint_prismatic(val):
+    if val > Pmax:
+    	return Pmax
+    elif val < Pmin:
+    	return Pmin
+    else:
+    	return val
+
+def constraint_revolute(val):
+    if val > Rmax:
+    	return Rmax
+    elif val < Rmin:
+    	return Rmin
+    else:
+    	return val
+    	
+# Arm publisher
 def arm_pub():
     pub = rospy.Publisher("arm_joints", Int16MultiArray, queue_size=10)
     rospy.init_node("arm_pub", anonymous=True)
 
     # Get arm values from arguments
     try:
-        arm_val1 = int(sys.argv[1])
+        arm_val1 = constraint_prismatic(int(sys.argv[1]))
     except:
-        arm_val1 = 0
+        arm_val1 = Pmin
     try:
-        arm_val2 = int(sys.argv[2])
+        arm_val2 = constraint_revolute(int(sys.argv[2]))
     except:
         arm_val2 = 0
     # Create an Int16MultiArray message
