ec7f5b6873094b42ab2d3773888230e4b14d2df6
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-06-08 12:04:08 +0300

    read json file and return task

--- scripts/jsonToTask.py
+++ scripts/jsonToTask.py
@@ -5,16 +5,18 @@ class Task:
     def __init__(self, task_dict):
         # Attributes are not clear yet
         self.id = task_dict["id"]
-        self.label = task_dict["label"]
+        self.name = task_dict["name"]
         self.map = task_dict["map"]
-        self.pickup_node = task_dict["pickup_node"]
-        self.dropoff_node = task_dict["dropoff_node"]
-        self.time = task_dict["time"]
+        self.pickupNode = task_dict["pickupNode"]
+        self.dropoffNode = task_dict["dropoffNode"]
+        self.time = task_dict["taskTime"]
 
     def __repr__(self):
         # TODO
-        ...
+        repr_str = f"id = {self.id},name = {self.name}, map = {self.map}, pickupNode = {self.pickupNode}, dropoffNode = {self.dropoffNode}, time = {self.time}"
+        return repr_str
 
 def readTask(file_path):
     with open(file_path, 'r') as file:
-        data = json.load(file)
+        data_dict = json.load(file)
+    return Task(data_dict)
